f93ce08bd6ddc769bf749cb08e8a22b3d1fb86eb
vamshicherala <vamshikrishna199904@gmail.com>
2021-10-16 09:12:17 +0530

    Welcome to GamblingStimulator

---
+++ src/com/workshop/Gambling.java
@@ -0,0 +1,17 @@
+package com.workshop;
+
+import java.util.Random;
+
+public class Gambling {
+
+    public static void main(String[] args) {
+        int stakePerDay = 100;
+        int betsPerGame = 1;
+        int wins = 0;
+        int game = 0;
+        int lose = 0;
+        System.out.println("**Welcome To Gambling**");
+        System.out.println("Everyday Stake: $"+stakePerDay);
+        System.out.println("bet on every game: $"+betsPerGame);
+    }
+}
